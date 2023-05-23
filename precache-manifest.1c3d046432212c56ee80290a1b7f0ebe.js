a16f60c603b109713b72f90da17ea0b0c8040d64
kucherenko <kucherenko.andrey@gmail.com>
2023-05-23 14:56:29 +0000

    Deploying to gh-pages from  @ f676e49c16b949356ee90e195b99ce348c1dcef5 🚀

---
+++ precache-manifest.1c3d046432212c56ee80290a1b7f0ebe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e51fb0abae8883ca62",
+    "url": "/css/app.49329b04.css"
+  },
+  {
+    "revision": "253998d59e2d37c1182e",
+    "url": "/css/chunk-vendors.5d27a9d6.css"
+  },
+  {
+    "revision": "c270c0dde8e471024f62131eb3b0e3e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45b222999fd7cb1df54f",
+    "url": "/js/about.07017290.js"
+  },
+  {
+    "revision": "c4e51fb0abae8883ca62",
+    "url": "/js/app.c8f17298.js"
+  },
+  {
+    "revision": "253998d59e2d37c1182e",
+    "url": "/js/chunk-vendors.5feec0c7.js"
+  },
+  {
+    "revision": "bd51f0dfe90d51a59a56ce26622a4c68",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

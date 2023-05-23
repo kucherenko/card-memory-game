327d524d619e3b31b71f641003f5da9c2d873a7c
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2023-05-23 17:51:02 +0300

    fix(tests): fixc unit tests

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="center grid">
-    <vs-row offset="1" v-for="row,index in size/4" :key="index">
-      <vs-col w="3" v-for="col in 4" :key="row+col">
+    <vs-row offset="1" v-for="(row, index) in size / 4" :key="index">
+      <vs-col w="3" v-for="col in 4" :key="row + col">
         <vs-card @click="pick">
           <template #img>
-            <img src="https://vuesax.com/foto5.png" alt="">
+            <img src="https://vuesax.com/foto5.png" alt="" />
           </template>
         </vs-card>
       </vs-col>
@@ -19,18 +19,16 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class extends Vue {
   @Prop() private size!: number;
 
-  array: any[];
+  private items: any[] = [];
 
   mounted() {
-    this.array = [
-      ...Array(Number(this.size)).keys()
-    ].concat(
-      ...Array(Number(this.size)).keys()
-    ).sort(() => 0.5 - Math.random());
+    this.items = [...Array(Number(this.size)).keys()]
+      .concat(...Array(Number(this.size)).keys())
+      .sort(() => 0.5 - Math.random());
   }
 
   pick() {
-    console.log(this.array);
+    console.log(this.items);
   }
 }
 </script>
